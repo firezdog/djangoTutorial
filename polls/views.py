fd983a448ec7af14a951b07665c049a1aa8b4fbc
Alex <firezdog@yahoo.com>
2018-07-02 20:59:08 -0500

    final non-generic version of poll app

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 # Create your views here.
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse 
-from .models import Question
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -15,8 +16,23 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
-    response = "You're looking at the results for question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/results.html', context)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': "You didn't select a choice."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    ##This code won't work, because I haven't narrowed down the choice to the particular question -- the counter will give the second choice for two different questions the same "id".
+    # choice_id = request.POST.get("choice")
+    # choice = get_object_or_404(Choice, pk=choice_id)
+    # choice.votes += 1
+    # choice.save()
+    # return redirect('polls:index')
